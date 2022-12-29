8e9baf288b8065636791a0d510403e2345920e95
Daniel Bojczuk <danielbojczuk@gmail.com>
2022-12-29 02:36:25 +0100

    Add testing/coverage to build

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 ROOT_DIR=$(pwd)
 
 rm -rf ./deploy/
@@ -6,6 +7,13 @@ rm -rf ./deploy/
 echo "### Building DistanceTrackerFunction ###"
 cd $ROOT_DIR/DistanceTrackerFunction/src
 dotnet restore
+dotnet build
+
+echo "### Testing DistanceTrackerFunction ###"
+cd $ROOT_DIR/DistanceTrackerFunction/test
+dotnet test /p:CollectCoverage=true /p:Threshold=\"90,90,90\" /p:ThresholdType=\"line,branch,method\"
+
+echo "### Publishing DistanceTrackerFunction ###"
 dotnet publish -c Release -r linux-arm64 --no-self-contained -o ../../deploy/DistanceTrackerFunction 
 
 echo "### Building Infrastrucutre ###"
