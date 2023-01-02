d7c8a12050ed9bc166d9a77f8e2b112c486c7998
Daniel Bojczuk <danielbojczuk@gmail.com>
2023-01-02 07:31:46 +0100

    Fix smoketest init

--- smoketest.sh
+++ smoketest.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 aws lambda invoke --function-name DevicesDistanceTrackerSmoketest ./test.out > output.txt
-grep FunctionError output.txt
-if [ "$?" -eq 0 ]; then
+
+if [ ! "$(grep -c FunctionError output.txt)" -eq "0" ]; then
+  echo "Error on test execution. Check Lambda function logs."
+  cat output.txt
   exit -1
 fi
