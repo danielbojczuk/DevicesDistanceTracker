38039b26d346336962230cd8577712722e6ef9fe
Daniel Bojczuk <danielbojczuk@gmail.com>
2023-01-02 02:14:04 +0100

    change

--- build.sh
+++ build.sh
@@ -9,19 +9,17 @@ cd $ROOT_DIR/DistanceTrackerFunction/src
 dotnet restore
 dotnet build
 
-# echo "### Testing DistanceTrackerFunction ###"
-cd $ROOT_DIR/DistanceTrackerFunction/test
-dotnet test
-
-echo "### Publishing DistanceTrackerFunction ###"
-cd $ROOT_DIR/DistanceTrackerFunction/src
-dotnet publish -c Release -r linux-arm64 --no-self-contained -o ../../deploy/DistanceTrackerFunction 
-
 echo "### Publishing DistanceTrackerFunctionSmoketest ###"
 cd $ROOT_DIR/DistanceTrackerFunctionSmoketest/src
-dotnet publish -c Release -r linux-arm64 --no-self-contained -o ../../deploy/DistanceTrackerFunctionSmoketest 
+dotnet restore
+dotnet build
+
+echo "### Testing DistanceTrackerFunction ###"
+cd $ROOT_DIR/DistanceTrackerFunction/test
+dotnet test
 
 echo "### Building Infrastrucutre ###"
 cd $ROOT_DIR/Infrastructure/src
 rm -rf .node_modules
 npm ci
+
